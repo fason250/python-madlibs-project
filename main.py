579891b03b79f9617850aa90b7c0db0eaf894481
fason250 <144928823+fason250@users.noreply.github.com>
2023-09-13 16:56:12 +0200

    Add files via upload
    this is the mdlib for beginners you by Jey fason

---
+++ main.py
@@ -0,0 +1,12 @@
+# this is madlib program made by  Jey Fason for learning purpose
+
+verb_one = input("Enter the first verb: ").lower()
+verb_two = input("Enter the second verb: ").lower()
+adjective = input("Enter the adjective: ").lower()
+famous_person = input("Enter the famous person's name: ").capitalize()
+
+madlib = f"Computer Programming is so {adjective}! It make me so excited all time because I love to \
+{verb_one}. Stay hydrated and {verb_two} like you are {famous_person}!"
+
+print("------------------------------")
+print(madlib)
